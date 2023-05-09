2a531217066f51b718480dc3d94e32a0b8d42b57
Arne Vogt <a.vogt@52north.org>
2023-05-09 17:24:43 +0000

    add example with postgres

---
+++ .devcontainer/create_tables.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS message (
+  id SERIAL PRIMARY KEY,
+  message varchar(250) NOT NULL
+);
--- app.py
+++ app.py
@@ -1,6 +1,38 @@
 from flask import Flask
+import psycopg2
+import os
+
 app = Flask(__name__)
+
+
 
 @app.route('/echo/<message>')
 def echo(message):
     return 'Your message was: ' + str(message)
+
+@app.route('/store/<message>')
+def store(message):
+    table_name = 'message'
+    message_column = 'message'
+    #connect to database
+    conn = psycopg2.connect(database="postgres", user="postgres",
+                        password="postgres", host="localhost", port="5432")
+    
+    #create cursor
+    cur = conn.cursor()
+    #insert message and commit
+    cur.execute('INSERT INTO ' + table_name + ' (message) VALUES(%s)', (message,))
+    conn.commit()
+
+    output = 'stored messages:' + '\n'
+    #select all stored messages
+    cur.execute('SELECT ' + message_column + ' FROM ' + table_name)
+    messages = cur.fetchall()
+    for row in messages:
+        output = output + row[0] + '\n'
+
+    #close resources
+    cur.close()
+    conn.close()
+
+    return output
